0a12f458e68e290062041300f0d912f2111ce52e
New Name <mubin.momin@nxp.com>
2020-05-20 10:15:21 -0500

    psuedo coded js

---
+++ script.js
@@ -0,0 +1,11 @@
+// document.ready set up to begin coding
+// script to place timer on the top using moment.js in text format
+
+// save button listener and time stamp
+
+// set items in local storage at save. 
+
+
+// load the local stoarge items at refresh for the items saved in each row of time slot. 
+
+// create function to assign whether the block of time is current, past, or future. 
