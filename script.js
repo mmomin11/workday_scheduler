c3a5f1e32b616207ff09562ef04061a286eacf52
New Name <mubin.momin@nxp.com>
2020-05-20 12:05:34 -0500

    styling

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 // document.ready set up to begin coding
 $(document).ready(function() {
+
 // script to place timer on the top using moment.js in text format
 $("#currentDay").text(moment().format('dddd') + ", " + moment().format("MMM Do YY"));
+
 // save button listener and time stamp
+$(".saveBtn").on("click", function () {
+    //get nearby values.
+    console.log(this);
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+
+    //set items in local storage.
+    localStorage.setItem(time, text);
+})
 
 // set items in local storage at save. 
 
